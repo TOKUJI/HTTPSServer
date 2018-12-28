bd950c3bb786264cc6e90855f60ade605e97dda0
TOKUJI <14055040+TOKUJI@users.noreply.github.com>
2018-12-28 13:19:51 +0000

    enabled to handle application/json

--- server/message.py
+++ server/message.py
@@ -1,34 +1,44 @@
 import http
 import re
 import sys
+import io
 from http.cookies import SimpleCookie
 
 # private source
-import util
-from logger import get_logger_set
+from .util import serializable
+from .logger import get_logger_set
 logger, log = get_logger_set('message')
 
+class BaseHTTPError(Exception):
+    def get_message(self):
+        if not self.status:
+            raise NotImplementedException()
 
-class BadRequest(Exception):
+        return '{} {}'.format(self.status.value, self.status.phrase)
+
+class BadRequest(BaseHTTPError):
     status = http.HTTPStatus.BAD_REQUEST
 
-class NotFound(Exception):
+class UnauthorizedError(BaseHTTPError):
+    status = http.HTTPStatus.UNAUTHORIZED
+        
+class NotFound(BaseHTTPError):
     status = http.HTTPStatus.NOT_FOUND
 
-class URITooLong(Exception):
+class URITooLong(BaseHTTPError):
     status = http.HTTPStatus.REQUEST_URI_TOO_LONG
 
-class RequestEntityTooLarge(Exception):
+class RequestEntityTooLarge(BaseHTTPError):
     status = http.HTTPStatus.REQUEST_ENTITY_TOO_LARGE
 
-class InternalServerError(Exception):
+class InternalServerError(BaseHTTPError):
     status = http.HTTPStatus.INTERNAL_SERVER_ERROR
 
-class MediaType(util.serializable):
+class MediaType(serializable):
     re = r'(\S+?)/(\S+?) ?; ?(\S+?=\S+)'
     pass
 
-class RequestLine(util.serializable):
+class RequestLine(serializable):
     re = r'(\S+) (\S+) (\S+)\r\n'
 
     def __init__(self, method, uri, version):
@@ -55,12 +65,12 @@ class RequestLine(util.serializable):
         return self.save()
 
 
-class Header(util.serializable):
+class Header(serializable):
     """ Indicates a header in HTTP-message.
     you can access via key, value 
     """
 
-    re = r'(\S+?):(.+?)\r\n'
+    re = r'(\S+?): +(.+?)\r\n'
     def __init__(self, key=None, value=None):
         super(Header, self).__init__()
         self.key = key
@@ -88,34 +98,33 @@ class Header(util.serializable):
         return self.key == None
 
     def save(self):
-        return '{}:{}\r\n'.format(self.key, self.value)
+        return '{}: {}\r\n'.format(self.key, self.value)
 
 
-class Headers(dict, util.serializable):
+class Headers(dict, serializable):
     def __init__(self, *args, cookie=SimpleCookie(), **kwds):
-        print(cookie)
         super(dict, self).__init__()
-        self.cookie = SimpleCookie()
- 
-    def set_cookie(self, text):
-        logger.info(text)
-        logger.info(self.cookie)
-        self.cookie.load(text)
+        self.cookie = cookie
 
-    def get_cookie(self):
-        return self.cookie.output()
+    @log 
+    def set_cookie(self, key, value):
+        self.cookie[key] = value
+
+    def get_cookie(self, key):
+        return self.cookie[key]
 
     def save(self):
-        logger.info(self.cookie)
         res = []
-        for k, v in self:
+        for k, v in self.items():
             res.append(Header(k,v).save())
             logger.info(res)
-        if res:
-            text = '\r\n'.join(*res)
-        else:
-            text = ''
-        return text + self.get_cookie() + '\r\n'
+
+        text = ''.join(res)
+
+        if self.cookie:
+            text += self.cookie.output() + '\r\n'
+
+        return text + '\r\n'
 
     @classmethod
     def load(cls, text):
@@ -125,15 +134,15 @@ class Headers(dict, util.serializable):
             if header.is_empty():
                 break
 
-            if header.key == 'Set-Cookie':
-                self.set_cookie(header.value)
+            if header.key == 'Cookie':
+                headers.cookie.load(header.value)
             else:
                 headers[header.key] = header.value
 
         return headers, text
 
 
-class StatusLine(util.serializable):
+class StatusLine(serializable):
     def __init__(self, version, StatusLine):
         self.version = version
         self.code = StatusLine.value
@@ -143,7 +152,7 @@ class StatusLine(util.serializable):
         return '{} {} {}\r\n'.format(self.version, self.code, self.reason)
 
 
-class ResponseBody(util.serializable):
+class ResponseBody(serializable):
     """docstring for ResponseBody"""
     re = r'(.*)'
     def __init__(self, str_):
@@ -158,7 +167,7 @@ class ResponseBody(util.serializable):
         return self.data
         
 
-class RequestBody(util.serializable):
+class RequestBody(serializable):
     """docstring for RequestBody"""
     re = r'[\r\n]*(.+?)=([^&\?]+)&?'
     def __init__(self, input_=None):
@@ -198,8 +207,26 @@ class RequestBody(util.serializable):
  
         return '\r\n' + res
 
+class RequestBodyJson(serializable):
+    def __init__(self, data):
+        super(RequestBodyJson, self).__init__()
+        self.data = data
 
-class HTTPMessage(util.serializable):
+    @classmethod
+    def load(cls, text):
+        import json
+        try:
+            data = json.loads(text)
+        except Exception as e:
+            logger.warning(e)
+            raise e
+        return cls(data), ''
+
+    def save(self):
+        return '\r\n' + json.dumps(self.data)
+
+
+class HTTPMessage(serializable):
     def __init__(self, start_line=None, headers={}, body=None):
         super(HTTPMessage, self).__init__()
         self.start_line = start_line
@@ -212,22 +239,51 @@ class HTTPMessage(util.serializable):
              & len(self.body) == 0
 
     @classmethod
-    def load(cls, str_, isResponse=True):
-        start_line, text = RequestLine.load(str_)
+    def load(cls, str_, isRequest=True):
+        try:
+            stream = io.StringIO(str_)
+            start_line, text = RequestLine.load(stream.readline())
 
-        headers, text = Headers().load(text)
+            x = stream.read().split('\r\n\r\n')
+            headers, text = Headers.load(x[0])
+        except Exception as e:
+            logger.error(e)
+            raise BadRequest()
 
-        # consider to use factory pattern or method
-        if isResponse:
-            body, text = RequestBody.load(text)
+        if not x[1]:
+            return cls(start_line, headers, ), x[1]
+
+        if isRequest:
+            t = 'Request'
         else:
-            body, text = ResponseBody.load(text)
+            t = 'Response'
+        logger.debug('isRequest = {}'.format(isRequest))
+
+        if 'Content-Type' in headers:
+            t2 = headers['Content-Type']
+            logger.debug(headers['Content-Type'])
+        else:
+            t2 = None
+
+        try:
+            body, text = _bodyClass[(t, t2)].load(x[1])
+        except Exception as e:
+            logger.error(e)
+            raise BadRequest()
 
         return cls(start_line, headers, body), text
 
     def save(self):
-        res = self.start_line.save() \
-            + self.headers.save() \
-            + self.body.save()
+        res = self.start_line.save() + self.headers.save()
+
+        if self.body:
+            res += self.body.save()
+
         return res
-# TODO!!!! create Headers class
+
+_bodyClass = {
+    ('Request', None): RequestBody,
+    ('Request', 'application/json'): RequestBodyJson,
+    ('Request', 'application/x-www-form-urlencoded'): RequestBody,
+    ('Response', None): ResponseBody,
+}
