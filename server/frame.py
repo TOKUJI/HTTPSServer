09445cbce8e50f63f711d2910d03e64d57c69980
TOKUJI <14055040+TOKUJI@users.noreply.github.com>
2019-01-06 12:04:53 +0000

    Successed to reply GET request from a curl client

--- server/frame.py
+++ server/frame.py
@@ -25,11 +25,16 @@ class FrameBase(object):
     """docstring for FrameBase"""
     factory = None
 
-    def __init__(self, length: int, type_, flags: bytes, stream_identifier: int):
+    def __init__(self, length: int, type_, flags: int, stream_identifier: int):
         self.length = length
         self.type_ = type_
         self.flags = flags
         self.stream_identifier = stream_identifier
+        logger.debug('type={}, '.format(self.type_) +\
+                     'flag={}, '.format(self.flags) +\
+                     'stream_identifier={} '.format(self.stream_identifier) +\
+                     'and payload size={}'.format(self.length))
+
 
     @classmethod
     def get_factory(cls):
@@ -38,25 +43,31 @@ class FrameBase(object):
         return cls.factory
 
     @classmethod
-    def create(cls, length, type_, flags, stream_identifier, data=None):
+    def create(cls, type_, flags, stream_identifier, data=None):
         factory = cls.get_factory()
+        if not data:
+            length = 0
+        else:
+            length = len(data)
+        logger.info('length of the data is {}'.format(length))
         return factory[type_](length, type_, flags, stream_identifier, data)
 
     @classmethod
-    def load(cls, frame):
-        length = int.from_bytes(frame[:3], 'big', signed=False)
-        type_ = frame[3:4]
-        flags = frame[4:5]
-        stream_identifier = int.from_bytes(frame[5:9], 'big', signed=False)
-
+    def load(cls, data):
         factory = cls.get_factory()
-        return factory[type_](length, type_, flags, stream_identifier, frame[9:])
+
+        length = int.from_bytes(data[:3], 'big', signed=False)
+        type_ = data[3:4]
+        flags = int.from_bytes(data[4:5], 'big', signed=False)
+        stream_identifier = int.from_bytes(data[5:9], 'big', signed=False)
+        payload = data[9:]
+        return factory[type_](length, type_, flags, stream_identifier, payload)
 
     def save(self):
         res = b''
         res += self.length.to_bytes(3, 'big', signed=False)
         res += self.type_
-        res += self.flags
+        res += self.flags.to_bytes(1, 'big', signed=False)
         res += self.stream_identifier.to_bytes(4, 'big', signed=False)
 
         return res
@@ -68,16 +79,17 @@ class FrameBase(object):
 
 class SettingFrame(FrameBase):
     """docstring for SettingFrame"""
-    def __init__(self, length: int, type_, flags: bytes, stream_identifier: int, data=None):
+    initial_window_size = None
+    def __init__(self, length: int, type_, flags: int, stream_identifier: int, data=None):
         super(SettingFrame, self).__init__(length, type_, flags, stream_identifier)
-        logger.debug('SettingFrame is called. flag={}, '.format(flags) +\
-                     'stream_identifier is {} '.format(stream_identifier) +\
-                     'and payload size is {}'.format(length))
+        logger.debug('SettingFrame is called.')
 
-        self.params = {b'\x00\x02': self.set_enable_push,
+        self.params = {b'\x00\x01': self.set_header_table_size,
+                       b'\x00\x02': self.set_enable_push,
                        b'\x00\x03': self.set_max_concurrent_streams,
                        b'\x00\x04': self.set_initial_window_size,
-                       } # TODO: handle other parameters
+                       b'\x00\x05': self.set_max_frame_size,
+                       }
 
         payload = BytesIO(data)
         while True:
@@ -88,7 +100,15 @@ class SettingFrame(FrameBase):
             value = payload.read(4)
             if len(value) != 4:
                 break
-            self.params[identifier](value)
+            try:
+                self.params[identifier](value)
+            except KeyError:
+                logger.error('unknown identifier: {}, {}'.format(identifier,
+                    int.from_bytes(value, 'big', signed=False)))
+
+    def set_header_table_size(self, value):
+        self.header_table_size = int.from_bytes(value, 'big', signed=False)
+        logger.debug('header_table_size: {}'.format(self.header_table_size))
 
     def set_enable_push(self, value):
         self.enable_push = int.from_bytes(value, 'big', signed=False)
@@ -102,8 +122,12 @@ class SettingFrame(FrameBase):
         self.max_concurrent_streams = int.from_bytes(value, 'big', signed=False)
         logger.debug('max_concurrent_streams: {}'.format(self.max_concurrent_streams))
 
+    def set_max_frame_size(self, value):
+        self.max_frame_size = int.from_bytes(value, 'big', signed=False)
+        logger.debug('max_frame_size: {}'.format(self.max_frame_size))
+
     def save(self):
-        base = super(SettingFrame, self).save()
+        base = super().save()
         # TODO: enable to alter settings parameters
         return base
 
@@ -116,9 +140,7 @@ class WindowUpdate(FrameBase):
     """docstring for WindowUpdate"""
     def __init__(self, length: int, type_, flags: bytes, stream_identifier: int, data=None):
         super(WindowUpdate, self).__init__(length, type_, flags, stream_identifier)
-        logger.debug('WindowUpdate is called. flag={}, '.format(flags) +\
-                     'stream_identifier is {} '.format(stream_identifier) +\
-                     'and payload size is {}'.format(length))
+        logger.debug('WindowUpdate is called.')
 
         payload = BytesIO(data)
         self.set_window_size(data)
@@ -136,19 +158,22 @@ class WindowUpdate(FrameBase):
     def FrameType():
         return FrameTypes.WINDOW_UPDATE
 
-class Headers(FrameBase):
+class HeadersFlags(Enum):
+    END_STREAM = 0x1
+    END_HEADERS = 0x4
+    PADDED = 0x8
+    PRIORITY = 0x20
+
+
+class Headers(FrameBase, dict):
 
     def __init__(self, length: int, type_, flags: bytes, stream_identifier: int, data=None):
         super(Headers, self).__init__(length, type_, flags, stream_identifier)
-        logger.debug('Headers is called. flag={}, '.format(flags) +\
-                     'stream_identifier is {} '.format(stream_identifier) +\
-                     'and payload size is {}'.format(length))
-        int_flags = int.from_bytes(flags, 'big', signed=False)
-
-        self.end_stream = 0x1 & int_flags
-        self.end_headers = 0x4 & int_flags
-        self.padded = 0x8 & int_flags
-        self.priority = 0x20 & int_flags
+        logger.debug('Headers is called.')
+        self.end_stream = HeadersFlags.END_STREAM.value & self.flags
+        self.end_headers = HeadersFlags.END_HEADERS.value & self.flags
+        self.padded = HeadersFlags.PADDED.value & self.flags
+        self.priority = HeadersFlags.PRIORITY.value & self.flags
         logger.debug('{}, {}, {}, {}'.format(self.end_stream,
                                              self.end_headers,
                                              self.padded,
@@ -160,18 +185,135 @@ class Headers(FrameBase):
             payload.read(1)
 
         if self.priority: # TODO: handle priority properly
-            self.stream_dependency = payload.read(4)
-            self.priority_weight = payload.read(1)
+            self.stream_dependency = int.from_bytes(payload.read(4), 'big', signed=False)
+            self.priority_weight = int.from_bytes(payload.read(1), 'big', signed=False)
             logger.debug('stream_dependency: {}, '.format(self.stream_dependency) +\
-                         'priiority_weight: {}'.format(self.priority_weight))
+                         'priority_weight: {}'.format(self.priority_weight))
 
-        self.set_header(payload.read())
+        if length:
+            decoder = Decoder()
+            fields = decoder.decode(payload.read())
+            for k, v in fields:
+                self[k] = v
+                logger.debug('{}: {}'.format(k, v))
 
-    def set_header(self, value):
+    def save(self):
+
+        encoder = Encoder()
+        payload = encoder.encode(self)
+        self.length = len(payload)
+
         decoder = Decoder()
-        self.header = decoder.decode(value)
-        logger.debug(self.header)
+        check = decoder.decode(payload)
+        logger.info('payload of the header is {}'.format(check))
+
+        base = super().save()
+        return base + payload
 
     @staticmethod
     def FrameType():
         return FrameTypes.HEADERS
+
+
+class GoAway(FrameBase):
+    def __init__(self, length: int, type_, flags: int, stream_identifier: int, data=None):
+        super().__init__(length, type_, flags, stream_identifier)
+        logger.debug('GoAway is called.')
+
+        payload = BytesIO(data)
+        self.stream_identifier = int.from_bytes(payload.read(4), 'big', signed=False)
+        self.error_code = int.from_bytes(payload.read(4), 'big', signed=False)
+        self.append_data = payload.read()
+        logger.debug('stream_identifier: {}'.format(self.stream_identifier))
+        logger.debug('error_code: {}'.format(self.error_code))
+        logger.debug('append_data: {}'.format(self.append_data))
+
+    @staticmethod
+    def FrameType():
+        return FrameTypes.GOAWAY
+
+class RstStream(FrameBase):
+    def __init__(self, length: int, type_, flags: int, stream_identifier: int, data=None):
+        super().__init__(length, type_, flags, stream_identifier)
+        logger.debug('RstStream is called.')
+        if len(data) != 4:
+            raise Exception('Frame size error')
+
+        self.error_code = int.from_bytes(data, 'big', signed=False)
+        logger.debug('error_code: {}'.format(self.error_code))
+
+    @staticmethod
+    def FrameType():
+        return FrameTypes.RST_STREAM
+
+
+
+class DataFlags(Enum):
+    END_STREAM = 0x1
+    PADDED = 0x8
+
+class Data(FrameBase):
+    """docstring for Data"""
+    def __init__(self, length: int, type_, flags: int, stream_identifier: int, data=None):
+        super().__init__(length, type_, flags, stream_identifier)
+        logger.debug('Data is called.')
+        self.end_stream = DataFlags.END_STREAM.value & self.flags
+        self.padded = DataFlags.PADDED.value & self.flags
+
+        payload = BytesIO(data)
+
+        if self.padded:
+            pad_length = int.from_bytes(payload.read(1), 'big', signed=False)
+            # TODO: add checking logic of pad_length.
+            data_length = length - pad_length - 1
+        else:
+            data_length = length
+
+        self.data = payload.read(data_length)
+
+    def save(self):
+        base = super().save()
+        logger.info(base)
+        logger.info('payload size of the data is {}'.format(len(self.data)))
+        logger.info(self.data)
+        return base + self.data
+
+    @staticmethod
+    def FrameType():
+        return FrameTypes.DATA
+
+
+class Priority(FrameBase):
+    def __init__(self, length: int, type_, flags: int, stream_identifier: int, data=None):
+        super().__init__(length, type_, flags, stream_identifier)
+        logger.debug('Priority is called.')
+
+        payload = BytesIO(data)
+
+        self.dependent_stream = int.from_bytes(payload.read(4), 'big', signed=False)
+        self.weight = int.from_bytes(payload.read(1), 'big', signed=False)
+
+    def save(self):
+        base = super().save()
+
+        payload = self.dependent_stream.to_bytes(4, 'big', signed=False) +\
+               self.weight.to_bytes(1, 'big', signed=False)
+        self.length = 5
+
+        return base + payload
+
+    @staticmethod
+    def FrameType():
+        return FrameTypes.PRIORITY
+        
+
+
+class Stream(object):
+    def __init__(self, parent, weight, window_size=None):
+        self.parent = parent
+        self.weight = weight
+        if window_size:
+            self.window_size = window_size
+
+    def __setattr__(self, key, value):
+        pass
--- server/server.py
+++ server/server.py
@@ -5,6 +5,7 @@ import ssl
 import sys
 from http import HTTPStatus
 from datetime import timezone, datetime
+from enum import Enum, auto
 # from urllib.parse import urlparse, parse_qs
 
 # private library
@@ -12,80 +13,51 @@ from datetime import timezone, datetime
 from . import message
 from . import util
 from .rsock import create_socket
-from .frame import FrameBase, FrameTypes, SettingFrame
+from .frame import FrameBase, FrameTypes, SettingFrame, HeadersFlags, DataFlags
 
 from .logger import get_logger_set
 logger, log = get_logger_set('server')
 
 
+class HandlerTypes(Enum):
+    HTTP1_1 = auto()
+    HTTP2 = auto()
+        
 class HandlerBase(object):
-    pass
-
-class HTTP1_1Handler(HandlerBase):
-    pass
-
-class HTTP2Handler(HandlerBase):
-    """docstring for HTTP2Server"""
-    def __init__(self, reader, writer):
+    def __init__(self, router, reader, writer):
+        self.router = router
         self.reader = reader
         self.writer = writer
 
-    async def run(self):
-        frame = await self.parse_stream()
-        await self.handle_frame(frame)
+    async def handle_request(self, path):
+        """ Handles HTTP request method. Call an appropriate function from path and method."""
+        raise NotImplementedException()
 
-        my_settings = FrameBase.create(0, FrameTypes.SETTINGS.value, b'\x00', frame.stream_identifier + 1)
-        await self.send_frame(my_settings)
 
-        frame = await self.parse_stream()
-        await self.handle_frame(frame)
+    @classmethod
+    def find_handler(cls, handler_type):
+        handlers = {klass.handler_type(): klass for klass in cls.__subclasses__()}
+        return handlers[handler_type]
+    
+    @staticmethod
+    def hander_type():
+        raise NotImplementedException('Handler must implement handler_type() function')
 
-        frame = await self.parse_stream()
-        await self.handle_frame(frame)
 
-    async def parse_stream(self):
-        data = await self.reader.read(8000) # get setting frame.
-        frame = FrameBase.load(data)
-        return frame
-        
-    async def handle_frame(self, frame):
-        if frame.FrameType() == FrameTypes.HEADERS:
-            pass
-        elif frame.FrameType() == FrameTypes.SETTINGS:
-            self.initial_window_size = frame.initial_window_size
-        elif frame.FrameType() == FrameTypes.WINDOW_UPDATE:
-            self.window_size = frame.window_size
-            # res = FrameBase.create(0, FrameTypes.SETTINGS.value, b'\x01', frame.stream_identifier + 1)
-            # self.writer.write(res.save())
-            # await self.writer.drain()
+class HTTP1_1Handler(HandlerBase):
+    def __init__(self, router, reader, writer):
+        super(HTTP1_1Handler, self).__init__(router, reader, writer)
 
-    async def send_frame(self, frame):
-        self.writer.write(frame.save())
-        await self.writer.drain()
+    @staticmethod
+    def handler_type():
+        return HandlerTypes.HTTP1_1
 
-class MyHTTPServer(object):
-    """ HTTP Server class. When ssl_context or certfile is set,
-    this server runs as a HTTPS server.
-    """
-    def __init__(self, 
-                 router = util.RouteRecord(),
-                 handlers = HTTP1_1Handler(),
-                 *, ssl_context =None, certfile=None, keyfile=None, password=None, **kwds):
-
-        # Create TLS context
-        if ssl_context and certfile:
-            raise TypeError('SSLContext and certfile must not be set at the same time')
-
-        if ssl_context:
-            self.ssl = ssl_context
-        elif certfile:
-            context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-            # context.set_alpn_protocols(['HTTP/1.1', 'h2']) # to enable HTTP/2, add 'h2'
-            context.load_cert_chain('server.crt', keyfile='server.key')
-            context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
-            self.ssl = context
-
-        self._route = router
+    @log
+    async def parse(self, data):
+        """ Parse HTTP/1.1 request """
+        text = data.decode('utf-8')
+        request = message.HTTPMessage.load(text)
+        return request
 
     def make_headers(self):
         x = [message.Header('Date', datetime.now(tz=timezone.utc).strftime(util.IMFFixdate)),
@@ -93,12 +65,6 @@ class MyHTTPServer(object):
              message.Header('Content-Type', 'text/html;charset=utf-8'),
             ]
         return message.Headers(headers=x)
-
-    def make_response(self, text):
-        status = message.StatusLine('HTTP/1.1', HTTPStatus.OK)
-        headers = self.make_headers()
-        body = message.ResponseBody.load(text)
-        return message.HTTPMessage(status, headers, body)
 
 
     def write_error(self, exception, writer):
@@ -112,23 +78,18 @@ class MyHTTPServer(object):
 
 
     @log
-    async def parse(self, data):
-        """ Parse HTTP/1.1 request """
-        text = data.decode('utf-8')
-        request = message.HTTPMessage.load(text)
-        return request
-
-    @log
-    async def handle_request(self, request, writer):
+    async def handle_request(self, request):
         """ Handle request and write the result to writer """
         try:
-            fn, methods = self._route.find(request.start_line.uri)
-            if not request.start_line.method in methods:
+            fn, methods = self.router.find(request.start_line.uri)
+            if request.start_line.method not in methods:
                 raise message.MethodNotAllowed()
 
-            logger.debug(fn)
+            if request.body:
+                response = await self.call_with_args(fn, request)
+            else:
+                response = await self.call_with_args(fn, None)
 
-            response = await self.call_with_args(fn, request)
             logger.debug(response)
             logger.debug(response.save())
 
@@ -136,29 +97,32 @@ class MyHTTPServer(object):
                 response = self.make_response(response)
 
             # append cookie
+            headers = self.make_headers()
             for k, v in request.headers.cookie.items():
-                if k not in response.headers.cookie:
-                    response.headers.set_cookie(k, v)
+                if k not in headers.cookie:
+                    headers.set_cookie(k, v)
 
             # append Content-Length header
             length = len(response.body.save().encode('utf-8'))
-            response.headers.set_header(message.Header('Content-Length', length))
+            headers.set_header(message.Header('Content-Length', length))
 
-            writer.write(response.save().encode('utf-8'))
+            response.headers = headers
+
+            self.writer.write(response.save().encode('utf-8'))
 
         except KeyError as e:
             logger.warning(e)
-            self.write_error(message.NotFound().with_traceback(sys.exc_info()[2]), writer)
+            self.write_error(message.NotFound().with_traceback(sys.exc_info()[2]), self.writer)
         except TypeError as e:
             logger.warning(e)
             e = message.InternalServerError().with_traceback(sys.exc_info()[2])
-            self.write_error(e, writer)
+            self.write_error(e, self.writer)
         except message.BaseHTTPError as e:
             e = e.with_traceback(sys.exc_info()[2])
             logger.warning(e)
-            self.write_error(e, writer)
+            self.write_error(e, self.writer)
 
-        await writer.drain()
+        await self.writer.drain()
 
     async def call_with_args(self, fn, request):
         """ If request body has some key-value pair and fn requires
@@ -168,7 +132,7 @@ class MyHTTPServer(object):
 
         sig = signature(fn)
         # delete undeclared parameters
-        if request.body:
+        if request:
             params = {k:v for k, v in request.body.data.items() if k in sig.parameters}
         else:
             params = {}
@@ -185,20 +149,127 @@ class MyHTTPServer(object):
         else:
             return res
 
+class HTTP2Handler(HandlerBase):
+    """docstring for HTTP2Server"""
+    def __init__(self, router, reader, writer):
+        super(HTTP2Handler, self).__init__(router, reader, writer)
+        self.client_stream_window_size = {}
+
+    async def run(self):
+        frame = await self.parse_stream()
+        my_settings = FrameBase.create(FrameTypes.SETTINGS.value, 0x0, frame.stream_identifier)
+        await self.send_frame(my_settings)
+        await self.handle_frame(frame)
+
+
+        while True:
+            frame = await self.parse_stream()
+            if not frame:
+                break
+            await self.handle_frame(frame)
+
+    async def parse_stream(self):
+        data = await self.reader.read(9)
+        if len(data) != 9:
+            return
+        payload = int.from_bytes(data[:3], 'big', signed=False)
+        data += await self.reader.read(payload)
+        return FrameBase.load(data)
+        
+    async def handle_request(self, header):
+        fn, methods = self.router.find(header[':path'])
+        if header[':method'] not in methods:
+            raise message.MethodNotAllowed()
+
+        reply_header = FrameBase.create(FrameTypes.HEADERS.value,
+                                        HeadersFlags.END_HEADERS.value,
+                                        header.stream_identifier)
+        reply_header[':status'] = HTTPStatus.OK.value
+        await self.send_frame(reply_header)
+
+        res = await fn()
+        res = res.encode('utf-8')
+        reply_data = FrameBase.create(FrameTypes.DATA.value,
+                                      DataFlags.END_STREAM.value,
+                                      header.stream_identifier,
+                                      res)
+        await self.send_frame(reply_data)
+
+    async def handle_frame(self, frame):
+        if frame.FrameType() == FrameTypes.HEADERS:
+            await self.handle_request(frame)
+
+        elif frame.FrameType() == FrameTypes.SETTINGS:
+            if frame.flags == 0x0:
+                await self.send_frame(FrameBase.create(FrameTypes.SETTINGS.value, 0x1, frame.stream_identifier))
+                if frame.initial_window_size:
+                    self.initial_window_size = frame.initial_window_size
+
+            elif frame.flags == 0x1:
+                logger.debug('Got ACK')
+
+        elif frame.FrameType() == FrameTypes.WINDOW_UPDATE:
+            if frame.stream_identifier == 0:
+                self.client_window_size = frame.window_size
+            else:
+                self.client_stream_window_size[frame.stream_identifier] = frame.window_size
+
+    async def send_frame(self, frame):
+        self.writer.write(frame.save())
+        await self.writer.drain()
+
+    @staticmethod
+    def handler_type():
+        return HandlerTypes.HTTP2
+
+
+class MyHTTPServer(object):
+    """ HTTP Server class. When ssl_context or certfile is set,
+    this server runs as a HTTPS server.
+    """
+    def __init__(self, 
+                 router = util.RouteRecord(),
+                 # handlers = HTTP1_1Handler(self._route, request, writer),
+                 *, ssl_context =None, certfile=None, keyfile=None, password=None, **kwds):
+
+        # Create TLS context
+        if ssl_context and certfile:
+            raise TypeError('SSLContext and certfile must not be set at the same time')
+
+        if ssl_context:
+            self.ssl = ssl_context
+        elif certfile:
+            context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+            context.set_alpn_protocols(['HTTP/1.1', 'h2']) # to enable HTTP/2, add 'h2'
+            context.load_cert_chain('server.crt', keyfile='server.key')
+            context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
+            context.options |= ssl.OP_NO_COMPRESSION
+            self.ssl = context
+
+        self._route = router
+
+    def make_response(self, text):
+        status = message.StatusLine('HTTP/1.1', HTTPStatus.OK)
+        body = message.ResponseBody.load(text)
+        return message.HTTPMessage(start_line=status, body=body)
+
+
     async def client_connected_cb(self, reader, writer):
         try:
-            request_data = await reader.read(8000)
-            if False and request_data[:24] == util.HTTP2:
-                logger.info('HTTP/2 connection is requested. {}'.format(len(request_data)))
-                http2 = HTTP2Handler(reader, writer)
+            request_data = await reader.read(24)
+            if request_data == util.HTTP2:
+                logger.info('HTTP/2 connection is requested.')
+
+                http2 = HandlerBase.find_handler(HandlerTypes.HTTP2)(self._route, reader, writer)
                 await http2.run()
 
             else:
                 if reader.at_eof():
                     raise message.RequestEntityTooLarge().with_traceback(sys.exc_info()[2])
 
-                request = await self.parse(request_data)
-                res = await self.handle_request(request, writer)
+                handler = HandlerBase.find_handler(HandlerTypes.HTTP1_1)(self._route, reader, writer)
+                request = await handler.parse(request_data)
+                response = await handler.handle_request(request)
 
         except Exception as e:
             self.write_error(e, writer)
